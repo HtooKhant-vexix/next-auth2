280cc0d795b3089f9045a6e2327907c5e809d6d9
HtooKhant-vexix <vexixongaming02@gmail.com>
2024-04-26 21:56:46 +0630

    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div className="text-red-300">page</div>;
+  return <div className="text-red-300">ppppp</div>;
 };
 
 export default page;
